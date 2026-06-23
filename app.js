73380c9e8ebd0d487bcaa6d4db12a6060e831f58
Eyad Gad <eyademg@yahoo.com>
2026-06-23 08:29:47 -0400

    Refresh website with 2026 end-date grid

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 ﻿const DATA_DIR = "assets/grid_search_20260618_185759";
-const DATA_VERSION = "final-20260619-grid-refresh-1";
+const DATA_VERSION = "final-20260623-grid-refresh-1";
 const dataFile = (file) => `${DATA_DIR}/${file}?v=${DATA_VERSION}`;
 const FILES = {
   ranked: dataFile("grid_validation_ranked_report.csv"),
